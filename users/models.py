0c8a9d6a6adf88aaadfe0e0b54a37a420dc5fbfa
Klimchuk-sergei <klim.work@mail.ru>
2025-09-01 20:14:25 +0300

    созданы модели user и superuser, сделаны первые миграции в БД

---
+++ users/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from .managers import UserManager
+
+
+class User(AbstractBaseUser):
+    """убираем стандартное поле ввода username"""
+    username = None
+    email = models.EmailField(unique=True, verbose_name="Email")
+    avatar = models.ImageField(upload_to="users_avatar", verbose_name='Аватар', null=True, blank=True)
+    phone_number = models.CharField(max_length=35, verbose_name='Номер телефона', null=True, blank=True)
+    country = models.CharField(max_length=100, verbose_name='Страна', null=True, blank=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+objects = UserManager()
+
+class Meta:
+    verbose_name = 'Пользователь'
+    verbose_name_plural = 'Пользователи'
+
+def __str__(self):
+    return self.email
